2918f6e1945e32044052c81627f8f53d2326be61
ossygarcia <mrbruckessy@gmail.com>
2023-02-11 22:13:52 +0100

    feature

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import Header from '../../components/header/Header';
 import Navbar from '../../components/navbar/Navbar';
+import './List.scss';
 
 const List = () => {
 	return (
 		<div>
 			<Navbar />
 			<Header type='list' />
+			<div className='list-container'>
+				<div className='list-wrapper'>
+					<div className='search-panel'>
+						<div className='listItems'>
+							<label>Destination</label>
+							<input type='text' />
+						</div>
+						<div className='searchForm'>
+							<label>Destination</label>
+							<input type='text' />
+						</div>
+					</div>
+					<div className='search-list'>Result</div>
+				</div>
+			</div>
 		</div>
 	);
 };
